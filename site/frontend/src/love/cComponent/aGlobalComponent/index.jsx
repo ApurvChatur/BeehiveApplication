864bc101f8782691870cce8d3472f385bd1978c7
ApurvChatur <chaturapurv@gmail.com>
2024-04-10 13:12:33 +0530

    Auth & Profile Fixed

--- site/frontend/src/love/cComponent/aGlobalComponent/index.jsx
+++ site/frontend/src/love/cComponent/aGlobalComponent/index.jsx
@@ -13,7 +13,9 @@ const GlobalComponent = ({ Redux, LogoutAPICall, children }) => {
       <Suspense fallback={null}>
         <HeaderComponent Redux={Redux} LogoutAPICall={LogoutAPICall} />
       </Suspense>
-      { children }
+      <div className='min-h-screen bg-[#ADEFD1] dark:bg-[#00203F] text-[#00203F] dark:text-[#ADEFD1]'>
+        { children }
+      </div>
       <Suspense fallback={null}>
         <FooterComponent Redux={Redux} />
       </Suspense>
--- site/frontend/src/love/cComponent/dAuthenticatedComponent/children/aAuthFormComponent/index.jsx
+++ site/frontend/src/love/cComponent/dAuthenticatedComponent/children/aAuthFormComponent/index.jsx
@@ -19,13 +19,13 @@ const AuthFormComponent = ({ Data, Redux, ReduxUltimate }) => {
         <div className="container px-5 py-24 mx-auto">
           <div className="flex flex-col text-center w-full mb-4">
             <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4">{Data.title}</h1>
-            <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify.</p>
+            <p className="lg:w-2/3 mx-auto leading-relaxed text-base">{Data?.subtitle}</p>
           </div>
 
           <div className="lg:w-1/2 md:w-2/3 mx-auto mb-4">
             <Alert variant="custom" >
               <RocketIcon className="h-4 w-4" />
-              <AlertTitle>Readymade User!</AlertTitle>
+              <AlertTitle>Try Demo User!</AlertTitle>
               <AlertDescription>
                 <p><b>Email:</b> shraddha.kapoor@beehive.com</p> 
                 <p><b>Password:</b> Shraddha@123</p>
@@ -47,7 +47,18 @@ const AuthFormComponent = ({ Data, Redux, ReduxUltimate }) => {
                         <div className="relative">
                           <label className="leading-7 text-sm">{each.label}</label>
                           <input 
-                            className="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-indigo-500 focus:bg-gray-900 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" 
+                            className="
+                              w-full bg-[#ADEFD1] dark:bg-[#00203F] bg-opacity-40 rounded border 
+                              border-[#00203F] dark:border-[#ADEFD1]
+                              focus:border-[#ADEFD1] dark:focus:border-[#00203F] 
+                              focus:bg-[#00203F] dark:focus:bg-[#ADEFD1] 
+                              focus:text-[#ADEFD1] dark:focus:text-[#00203F] 
+                              focus:ring-2 
+                              focus:ring-[#00203F] dark:focus:ring-[#ADEFD1] 
+                              text-[#00203F] dark:text-[#ADEFD1] 
+                              text-base outline-none 
+                              py-1 px-3 leading-8 transition-colors duration-200 ease-in-out
+                            " 
                             type={each.type}
                             name={each.name} 
                             placeholder={each.placeholder} 
@@ -72,7 +83,7 @@ const AuthFormComponent = ({ Data, Redux, ReduxUltimate }) => {
                   }) }
                 </div>
 
-                <div className="p-2 w-full pt-2 mt-4 border-t border-gray-800 text-center">
+                <div className="p-2 w-full pt-2 mt-4 border-t border-[#00203F] dark:border-[#ADEFD1] text-center">
                   {Data.links.map((each, index) => {
                     return (
                       <React.Fragment key={index}>
